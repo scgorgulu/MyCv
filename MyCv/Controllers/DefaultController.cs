23512b0f6ac4d2ffe538397f6151abcc7a32a123
Serhat Cagri Gorgulu <cagri.gorgulu@gazi.edu.tr>
2023-03-03 01:48:05 +0300

    Add project files.

---
+++ MyCv/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
